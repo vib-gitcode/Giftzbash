0d0d22938dda90c13f4e8d9e9298e89ad9de67b4
Vibnet <vibhav68@outlook.com>
2025-09-14 21:43:49 -0500

    Delete utils.js

--- utils.js
+++
@@ -1,9 +0,0 @@
-// Simulated fetch and save using localStorage fallback
-async ;function loadJSON(filename) {
-  const data = localStorage.getItem(filename);
-  return data ? JSON.parse(data) : null;
-}
-
-async function saveJSON(filename, data) {
-  localStorage.setItem(filename, JSON.stringify(data, null, 2));
-}
